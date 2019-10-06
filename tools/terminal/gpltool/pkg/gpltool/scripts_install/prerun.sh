8da0d9f555202ff9cf2be711caa9f061dfe278c5
LaGvidilo <cyrilcoelho67@gmail.com>
2019-10-06 05:00:35 +0200

    terminal tool finish

---
+++ tools/terminal/gpltool/pkg/gpltool/scripts_install/prerun.sh
@@ -0,0 +1,9 @@
+echo version de python:
+python -c 'import sys; print(sys.version_info[0:2])'
+echo NOTE: la version 2_7 est requise et compatible
+python get-pip.py
+python -m pip install statistics
+python -m pip install sklearn
+python -m pip install numpy
+python -m pip install pytexit
+python -m pip install requests
