883d44e96200b05b644fdd3d234f8d82c78e2ed0
LaGvidilo <cyrilcoelho67@gmail.com>
2019-10-06 05:14:51 +0200

    add spellbook app

---
+++ tools/GUI/SpellBook/SpellBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpellBook
+//
+//  Created by Rick Sanchez on 06/10/2019.
+//  Copyright © 2019 Rick Sanchez. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
